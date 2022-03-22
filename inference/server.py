6f400d88ee6d1f1083ea449a87e1712265624a46
Bradford Gill <bradford42400@gmail.com>
2022-03-22 16:21:43 -0400

    ahhhhhh

--- inference/server.py
+++ inference/server.py
@@ -23,6 +23,7 @@ app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
 # define celery
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 # set up nsc2 and camera only if name is main
 if __name__ == "__main__":

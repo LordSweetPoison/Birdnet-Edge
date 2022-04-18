3647ad0ea610899d5acc48931607e00298f85431
Bradford Gill <bradfordgill@umass.edu>
2022-04-18 15:48:08 -0400

    python

--- inference/servertpu.py
+++ inference/servertpu.py
@@ -51,7 +51,7 @@ def async_upload_photo(image, objects):
     
     # post full photo
     filepath =  'images/' + photo_name + extention
-    print(S3.upload_fileobj(to_post, S3_BUCKET, filepath))
+    S3.upload_fileobj(to_post, S3_BUCKET, filepath)
 
     return None
 
@@ -73,7 +73,7 @@ def run():
 
         # if the list of birds (objects) is not empty, upload the photo 
         if objects.size > 0:
-            print('here')
+            print('Bird found')
             async_upload_photo(frame, objects)
 
 if __name__ == '__main__':
